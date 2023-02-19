6821c73c7db55c5238dc8972052206e154b8dfd4
Jack Lockett <32436468+jacklockett@users.noreply.github.com>
2023-02-19 22:16:20 +0000

    Update clientSettings.php

--- a4a/app/Views/clientSettings.php
+++ a4a/app/Views/clientSettings.php
@@ -20,6 +20,8 @@
 
             <br>
 
+            <div style="border: 1px solid #99A3A4;">
+
             <div class="ibox-title">
                 <h2>My Account Settings</h2>
             </div>
@@ -27,14 +29,21 @@
             <div class="ibox-content">
 
             <a class="btn btn-outline btn-secondary" href="<?php echo base_url(); ?>/AdminController/pwordReset" role="button"> <i
+                            class="fa fa-cog"></i> Change Details</a>
+
+            <a class="btn btn-outline btn-secondary" href="<?php echo base_url(); ?>/AdminController/pwordReset" role="button"> <i
                             class="fa fa-cog"></i> Change Password</a>
 
-                            <a class="btn btn-outline btn-danger" href="<?php echo base_url(); ?>/AdminController/adminDeleteAccount" role="button"> <i
-                            class="fa fa-trash-o"></i> Delete My Account</a>
+            <a class="btn btn-outline btn-secondary" href="<?php echo base_url(); ?>/AdminController/pwordReset" role="button"> <i
+            class="fa fa-user"></i> Change Profile Picture</a>
+            </div>
+
             </div>
             
 
             <br>
+
+            <div style="border: 1px solid #3498DB;">
 
             <div class="ibox-title">
                 <h2>User Settings</h2>
@@ -48,6 +57,32 @@
                     <a class="btn btn-success btn-outline" href="<?php echo base_url(); ?>/AdminController/addUser" role="button"> <i
                             class="fa fa-plus "></i> Add Admin User(s)</a>
 
+                </p>
+                <div>
+            </div>
+
+            </div>
+
+</div>
+            
+        </div>
+
+        <br>
+
+        <div style="border: 1px solid red;">
+
+        <div class="ibox-title">
+                <h2>Danger Zone</h2>
+            </div>
+
+            <div class="ibox-content">
+
+            <div>
+
+                <p>
+                <a class="btn btn-outline btn-danger" href="<?php echo base_url(); ?>/AdminController/adminDeleteAccount" role="button"> <i
+                            class="fa fa-trash-o"></i> Delete My Account</a>
+
                     <a class="btn btn-outline btn-danger" href="<?php echo base_url(); ?>/AdminController/adminDeleteUser" role="button"> <i
                             class="fa fa-trash-o"></i> Delete Admin User(s)</a>
 
@@ -56,10 +91,8 @@
                 </p>
                 <div>
             </div>
-
-            </div>
-        </div>
     </div>
+</div>
 </div>
 
 <?= view('templates/footer'); ?>
